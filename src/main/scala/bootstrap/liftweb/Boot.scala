079e7b3435daba9bd098065d6f005453b29e6202
Piotr Dyraga <piotr.dyraga@softwaremill.com>
2014-10-18 14:05:16 +0200

    Initial commit - blank Lift project

---
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -0,0 +1,35 @@
+package bootstrap.liftweb
+
+import net.liftweb._
+import util._
+import Helpers._
+
+import common._
+import http._
+import sitemap._
+import Loc._
+import net.liftmodules.JQueryModule
+import net.liftweb.http.js.jquery._
+
+class Boot {
+  def boot {
+    // where to search snippet
+    LiftRules.addToPackages("code")
+
+    //Show the spinny image when an Ajax call starts
+    LiftRules.ajaxStart =
+      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
+
+    // Make the spinny image go away when it ends
+    LiftRules.ajaxEnd =
+      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
+
+    // Force the request to be UTF-8
+    LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
+
+    //Init the jQuery module, see http://liftweb.net/jquery for more information.
+    LiftRules.jsArtifacts = JQueryArtifacts
+    JQueryModule.InitParam.JQuery=JQueryModule.JQuery191
+    JQueryModule.init()
+  }
+}
---
+++ src/main/scala/code/snippet/HelloWorld.scala
@@ -0,0 +1,17 @@
+package code
+package snippet
+
+import net.liftweb.util.Helpers._
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+class HelloWorld {
+
+  private val dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+
+  def hello = {
+    "#time *" #> dateFormat.format(new Date())
+  }
+}
+
