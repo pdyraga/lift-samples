c76ef2d7cbda391b6073991c4016e4cbbd92eb38
Piotr Dyraga <piotr.dyraga@softwaremill.com>
2014-10-20 13:31:51 +0200

    Implicit bindings for Scala and Lift futures

--- src/main/scala/code/snippet/HelloWorld.scala
+++ src/main/scala/code/snippet/HelloWorld.scala
@@ -1,17 +1,25 @@
 package code
 package snippet
 
-import net.liftweb.util.Helpers._
-
 import java.text.SimpleDateFormat
 import java.util.Date
 
+import code.lib.FutureBinds._
+import net.liftweb.actor.LAFuture
+import net.liftweb.util.Helpers._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
 class HelloWorld {
 
-  private val dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+  def render = {
+    "#scala-future *" #> Future { Thread.sleep(5000); date } &
+    "#lift-lafuture *" #> LAFuture.build { Thread.sleep(6000); date }
+  }
 
-  def hello = {
-    "#time *" #> dateFormat.format(new Date())
+  private def date: String = {
+    new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date())
   }
 }
 
